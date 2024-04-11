17d30c86810a7282a0640d1e48af87a32066224d
FranceHuon <133089058+FranceHuon@users.noreply.github.com>
2024-04-11 22:36:12 +0200

    Modification Footer.js, App.js et Banner.js

--- la-maison-jungle/src/components/App.js
+++ la-maison-jungle/src/components/App.js
@@ -1,17 +1,22 @@
 import Banner from './Banner'
+import logo from '../assets/logo.png'
 // import Cart from './Cart'
-import ShoppingList from './ShoppingList';
-import QuestionForm from './QuestionForm';
+import Footer from './Footer'
+import ShoppingList from './ShoppingList'
 
 function App() {
-  return (
-    <div>
-      <Banner />
-      {/* <Cart /> */}
-      <ShoppingList />
-      <QuestionForm />
-    </div>
-  );
+	return (
+		<div>
+			<Banner>
+				<img src={logo} alt='La maison jungle' className='lmj-logo' />
+				<h1 className='lmj-title'>La maison jungle</h1>
+			</Banner>
+
+			{/* <Cart /> */}
+			<ShoppingList />
+			<Footer />
+		</div>
+	)
 }
 
-export default App;
+export default App
--- la-maison-jungle/src/components/Banner.js
+++ la-maison-jungle/src/components/Banner.js
@@ -1,25 +1,7 @@
 import '../styles/Banner.css'
-import logo from '../assets/logo.png'
 
-function Banner() {
-    const title = 'La maison jungle'
-    const currentMonth = new Date().getMonth()
-    const isSpring = currentMonth >= 2 && currentMonth <=5
-    const recommendation =  isSpring ? (
-        <div>C'est le printemps, rempotez </div>
-    ) : (
-         <div>Ce n'est pas le moment de rempoter</div>
-    )
-
-    return (
-        <div className='lmj-banner'>
-            <div className='lmj-banner-row'>
-                <img src={logo} alt='La maison jungle' className='lmj-logo' />
-                <h1 className='lmj-title'>{title}</h1>
-            </div>
-            {recommendation}
-        </div>
-    )
+function Banner({ children }) {
+	return <div className='lmj-banner'>{children}</div>
 }
 
 export default Banner
