e507bd14d706ff99262cc4218fd8952bc2cb7a14
FranceHuon <133089058+FranceHuon@users.noreply.github.com>
2024-04-22 16:20:45 +0200

    Modification App.js, ShoppingList.js et Cart.js

--- la-maison-jungle/src/components/ShoppingList.js
+++ la-maison-jungle/src/components/ShoppingList.js
@@ -1,40 +1,52 @@
 import { plantList } from '../datas/plantList'
-import PlantItem from '../components/PlantItem'
+import PlantItem from './PlantItem'
 import '../styles/ShoppingList.css'
 
+function ShoppingList({ cart, updateCart }) {
+	const categories = plantList.reduce(
+		(acc, plant) =>
+			acc.includes(plant.category) ? acc : acc.concat(plant.category),
+		[]
+	)
 
-function ShoppingList() {
-    // Création d'un tableau contenant les catégories uniques de plantes
-    // Utilisation de la méthode reduce pour itérer sur la liste des plantes
-    const categories = plantList.reduce(
-        (acc, plant) =>
-         // Vérification si la catégorie de la plante est déjà présente dans le tableau d'accumulation (acc)
-            acc.includes(plant.category) ? acc : acc.concat(plant.category),
-        []
+	function addToCart(name, price) {
+		const currentPlantSaved = cart.find((plant) => plant.name === name)
+		if (currentPlantSaved) {
+			const cartFilteredCurrentPlant = cart.filter(
+				(plant) => plant.name !== name
+			)
+			updateCart([
+				...cartFilteredCurrentPlant,
+				{ name, price, amount: currentPlantSaved.amount + 1 }
+			])
+		} else {
+			updateCart([...cart, { name, price, amount: 1 }])
+		}
+	}
 
-    )
-
-    return (
-        <div>
-            <ul>
-                {categories.map((cat) => (
-                    <li key={cat}>{cat}</li>
-                ))}
-            </ul>
-            <ul className='lmj-plant-list'> 
-                {plantList.map(({id, cover, name, water, light }) => (
-                    <PlantItem
-                    id={id}
-                    cover={cover}
-                    name={name}
-                    water={water}
-                    light={light}
-                    />
-                ))}
-            </ul>
-        </div>
-    )
-
+	return (
+		<div className='lmj-shopping-list'>
+			<ul>
+				{categories.map((cat) => (
+					<li key={cat}>{cat}</li>
+				))}
+			</ul>
+			<ul className='lmj-plant-list'>
+				{plantList.map(({ id, cover, name, water, light, price }) => (
+					<div key={id}>
+						<PlantItem
+							cover={cover}
+							name={name}
+							water={water}
+							light={light}
+							price={price}
+						/>
+						<button onClick={() => addToCart(name, price)}>Ajouter</button>
+					</div>
+				))}
+			</ul>
+		</div>
+	)
 }
 
 export default ShoppingList
